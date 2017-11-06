ed85cbf0543c22016bae619cf37cc000dda7099d
Jaime RGP <jaime.rogue@gmail.com>
2017-11-06 14:15:57 +0100

    Update setup.py repo url

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ long_description = read('README.md')
 setup(
     name='gaudiview',
     version='0.0.1',
-    url='https://github.com/insilichem',
+    url='https://github.com/insilichem/gaudiview',
     author='Jaime Rodríguez-Guerra',
     author_email='jaime.rogue@gmail.com',
     description=long_description,
