bc7d30a6c86d2dc0ba4a13d1dd63291df408db90
Jaime RGP <jaime.rogue@gmail.com>
2019-02-01 17:00:22 +0100

    use versioneer

--- gaudiview/__init__.py
+++ gaudiview/__init__.py
@@ -30,3 +30,7 @@ and other metadata.
 
 *GaudiView uses UCSF Chimera, PyYaml, and tkintertable.*
 """
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
